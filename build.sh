15ebb007f66eb860e277b4ad16a8af3e13643c5a
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2015-10-12 19:51:51 +0200

    cmake

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -d "build" ] ; then
+  ./configure
+fi
+
+cd build
+
+make
+
+if [ "$?" != "0" ] ; then
+  echo "Could not build!"
+  exit 1
+fi
+
+make install
+
+cd ..
---
+++ src/supplesearch/document.hpp
@@ -0,0 +1,10 @@
+#ifndef ALJ_SUPPLESEARCH_DOCUMENT_HPP_
+#define ALJ_SUPPLESEARCH_DOCUMENT_HPP_
+
+namespace SuppleSearch {
+  class Document {
+
+  };
+}
+
+#endif
