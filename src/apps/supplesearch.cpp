859ba42308a4e228a1ada8b0ef5d978eec09d87d
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2015-10-14 17:09:21 +0200

    debug

--- src/apps/supplesearch.cpp
+++ src/apps/supplesearch.cpp
@@ -2,6 +2,8 @@
 #include <supplesearch/databases/text.hpp>
 #include <supplesearch/measures/cosine.hpp>
 
+#include <supplesearch/algorithms/inverse_document_frequency.hpp>
+
 #include <iostream>
 
 using namespace SuppleSearch;
@@ -16,6 +18,13 @@ int main(int argc, char** argv)
   Databases::Text::shared db(Databases::Text::build(argv[1]));
   Databases::Text::shared keywords(Databases::Text::build(argv[2]));
   Measure::shared measure(new Measures::Cosine());
+
+  // Algorithms::InverseDocumentFrequency idf(db);
+  // auto qt = idf.process(keywords->documents().front()->stemmed_content());
+  // int i = 0;
+  // for (auto stem : keywords->documents().front()->stemmed_content()) {
+  //   std::cout << qt(i++) << "\t" << stem << std::endl;
+  // }
 
   Engine engine(db, keywords, measure);
 
