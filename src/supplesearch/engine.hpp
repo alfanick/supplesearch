ce8080919d3792d78dc6b690d2ad3970de2a74d7
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2015-10-14 15:25:14 +0200

    doc engine

--- src/supplesearch/engine.hpp
+++ src/supplesearch/engine.hpp
@@ -7,18 +7,49 @@
 
 namespace SuppleSearch {
 
+/**
+ * List of search results. First value is score,
+ * second is the Document.
+ */
 using ResultList = std::vector<std::pair<double, Document::shared>>;
 
+/**
+ * Search engine working on given database and keywords
+ * with given measure.
+ */
 class Engine {
   public:
+    /**
+     * Create new Engine instance.
+     *
+     * @param database Available documents
+     * @param keywords Available keywords
+     * @param measure Used measure
+     */
     Engine(const Database::shared database, const Database::shared keywords, const Measure::shared measure);
 
+    /**
+     * Query Database.
+     *
+     * @param q Query
+     * @return Results
+     */
     ResultList query(const Document::shared q);
+
+    /**
+     * Query Database via string
+     *
+     * @param q Query
+     * @return Results
+     */
     ResultList query(const std::string q);
 
   private:
+    //! Used measure
     Measure::shared measure_;
+    //! Documents
     Database::shared database_;
+    //! Available keywords
     WordList keywords_;
 };
 
