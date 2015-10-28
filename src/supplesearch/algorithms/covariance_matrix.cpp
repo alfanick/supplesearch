197874dfb210752c0c92a88a68c2629d0aac5574
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2015-10-28 16:48:15 +0100

    cut

--- src/supplesearch/algorithms/covariance_matrix.cpp
+++ src/supplesearch/algorithms/covariance_matrix.cpp
@@ -14,7 +14,7 @@ std::vector<std::set<size_t>> CovarianceMatrix::find_best(const arma::vec old, s
   arma::vec possible(old);
 
   for (size_t i = 0; i < possible.size(); i++) {
-    if (vis.find(i) != vis.end() || old(i) < 0.1)
+    if (vis.find(i) != vis.end() || old(i) < 0.01)
       continue;
     arma::uword index;
     possible.max(index);
